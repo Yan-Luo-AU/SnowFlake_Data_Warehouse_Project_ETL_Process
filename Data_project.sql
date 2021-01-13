79ea0c3c00678992a278c718bd00c052edcfee93
Yan-Luo-AU <yanniiluo@gmail.com>
2021-01-13 12:08:20 +1100

    Add files via upload

---
+++ Data_project.sql
@@ -0,0 +1,222 @@
+use role sysadmin;
+drop database if exists data_project;
+create database data_project;
+
+create schema dev;
+
+create or replace table data_project.dev.src_video (
+    DateTime text,
+    VideoTitle text,
+    events text
+);
+
+--create file format
+create or replace file format data_project.dev.format_src_video
+type='csv'
+field_delimiter = ','
+skip_header =1
+ESCAPE = '\\'
+field_optionally_enclosed_by='"';
+
+--create an external s3 stage:
+create or replace stage data_project.DEV.stage_src_video
+url='s3://yan-luo-test-bucket-2020/data_project/'
+credentials=(AWS_KEY_ID='XXXXXXXXXXX' AWS_SECRET_KEY='XXXXXXXXXX')
+;
+
+--verify the stage is created:
+show stages;
+
+--create a pipe using auto_ingest=true
+create or replace pipe data_project.dev.pipe_src_video auto_ingest=true
+as copy into data_project.dev.src_video
+from @data_project.dev.stage_src_video
+file_format = (format_name = data_project.dev.format_src_video)
+;
+
+--verify the pipe is created using:
+show pipes;
+
+--check if the uploaded file is in stage location
+ls @data_project.dev.stage_src_video;
+
+--check pipe status
+select system$pipe_status('data_project.dev.pipe_src_video');
+
+--check the copy history
+select *
+from table(information_schema.copy_history(TABLE_NAME=>'src_video', START_TIME=> DATEADD(hours, -1, CURRENT_TIMESTAMP())));
+
+--Validate any loads for the pipe within the previous hour:
+select * from table(validate_pipe_load(
+  pipe_name=>'data_project.dev.pipe_src_video',
+  start_time=>dateadd(hour, -1, current_timestamp())));
+
+---Data auditing-----
+select max(length(datetime)), max(length(videotitle)), max(length(events)) from src_video;
+
+---validate the data----
+select count(*) from data_project.dev.src_video
+where contains(events,'206')
+union all
+select count(*) from data_project.dev.src_video
+where events like '%206%'
+union all
+select count(*) from data_project.dev.src_video
+;
+
+select * from data_project.dev.src_video
+where videotitle like '%\\%';
+
+select * from data_project.dev.src_video
+where ARRAY_SIZE(split(videotitle,'|')) = 1;
+
+select distinct split(videotitle,'|')[0] as platform
+from data_project.dev.src_video;
+
+
+select distinct SPLIT_PART(videotitle,'|',-1)as video_title
+from data_project.dev.src_video;
+
+
+select datetime::timestamp as Date_Time, minute(Date_Time) from data_project.dev.src_video limit 100;
+
+
+-----create a view to save the transformed data
+CREATE OR REPLACE VIEW data_project.dev.vw_src_video AS
+(
+select 
+    DateTime::timestamp as Date_Time,
+    year(Date_Time) as Year,
+    quarter(Date_time) as Quarter,
+    month(Date_Time) as Month,
+    week(Date_Time) as Week,
+    dayofweek(Date_Time) as Day_of_week,
+    hour(Date_Time) as Hour,
+    minute(Date_Time) as MINUTE,
+    case when split(videotitle,'|')[0] like '%iphone%' then 'iPhone'
+         when split(videotitle,'|')[0] like '%Android%' then 'Android Phone'
+         when split(videotitle,'|')[0] like '%ipad%' then 'iPad'
+    else 'Desktop'
+    end as platform,  
+    initcap(replace(split(videotitle,'|')[0],'""','')) as site, 
+    SPLIT_PART(videotitle,'|',-1) as video,
+    events
+from data_project.dev.src_video
+where events like '%206%' and ARRAY_SIZE(split(videotitle,'|')) >1
+  );
+  
+  
+  ---check the view
+  Select * from data_project.dev.vw_src_video limit 1000; 
+  
+  
+  ---create dimension table DimPlatform
+#DROP TABLE data_project.DEV.DimPlatform;
+
+CREATE OR REPLACE TABLE data_project.dev.DimPlatform
+(
+  Platform_Skey number AUTOINCREMENT PRIMARY KEY,
+  Platform VARCHAR2(200 BYTE) not null
+);
+----populate DimPlatform
+merge into data_project.dev.DimPlatform dest
+  using (select distinct Platform from data_project.dev.vw_src_video) src
+  on src.Platform = dest.Platform
+  when not matched
+  then insert (
+      Platform)
+      values (
+          src.Platform);
+          
+---create dimension table DimVideo
+DROP TABLE data_project.dev.DimVideo;
+
+CREATE OR REPLACE TABLE data_project.dev.DimVideo
+(
+  VideoTitle_Skey number AUTOINCREMENT PRIMARY KEY,
+  video VARCHAR2(200 BYTE) not null
+);
+
+merge into data_project.dev.DimVideo dest
+  using (select distinct video_title from data_project.dev.vw_src_video) src
+  on src.video = dest.video
+  when not matched
+  then insert (
+      video)
+      values (
+          src.video);
+
+---create dimension table DimSite
+
+#The DROP TABLE IF EXISTS data_project.DEV.DimSite;
+
+CREATE OR REPLACE TABLE data_project.DEV.DimSite
+(
+  Site_Skey number AUTOINCREMENT PRIMARY KEY,
+  site VARCHAR2(200 BYTE) not null
+);
+
+merge into data_project.dev.DimSite dest
+  using (SELECT DISTINCT site FROM data_project.dev.vw_src_video) src
+  on src.site = dest.site
+  when not matched
+  then insert (
+      site)
+      values (
+          src.site);
+          
+---create dimension table DimDate
+CREATE OR REPLACE TABLE data_project.DEV.DimDate
+(
+  DateTime_Skey number AUTOINCREMENT primary key,
+  Date_Time datetime not null,
+  Year int Not null,
+  Quarter int Not null,
+  Month int not null,
+  week int not null,
+  Day_of_week int not null,
+  hour int not null,
+  minute int not null
+);
+
+merge into data_project.dev.DimDate dest
+  using (SELECT DISTINCT Date_Time,Year, Quarter,Month,week,Day_of_week,hour,minute FROM data_project.dev.vw_src_video) src
+  on src.Date_Time = dest.Date_Time
+  when not matched
+  then insert (
+      Date_Time,Year, Quarter,Month,week,Day_of_week,hour,minute)
+      values (
+          src.Date_Time, src.Year, src.Quarter, src.Month, src.week, src.day_of_week, src.hour, src.minute);
+          
+----create fact table VideoStart_fact
+Create or replace table data_project.dev.VideoStart_fact(
+    DataTime_Skey Number AUTOINCREMENT primary key,
+    platform_Skey number not null,
+    video_Skey number not null,
+    site_Skey number not null,
+    event text not null
+    );
+
+
+
+merge into data_project.dev.VideoStart_fact dest
+    using (
+        select P.platform_Skey, D.DataTime_Skey, V.video_Skey, S.site_Skey, M.event from  data_project.dev.vw_src_video as M
+        left join data_project.dev.dimplatform as P on P.PLATFORM = M.PLATFORM
+        left join data_project.dev.dimvideo as V on V.Video = M.video
+        left join data_project.dev.dimSite as S on S.site = M.site
+    ) src
+    on src.platform_Skey = dest.platform_Skey
+    and src.DataTime_Skey = dest.DataTime_Skey
+    and src.video_Skey = dest.video_Skey
+    and src.site_Skey = dest.site_Skey
+    and src.events = dest.events
+     when not matched
+      then insert (
+          platform_Skey,DataTime_Skey, video_Skey,site_Skey,events)
+          values (
+              src.platform_Skey, src.DataTime_Skey, src.video_Skey, src.site_Skey, src.events);
+              
+ --select count(*) from data_project.dev.vw_src_video;
+    
